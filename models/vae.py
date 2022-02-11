3aa02ca12e851fb3350b297586b875f64735a919
molnarszilard <molnarszilard10@gmail.com>
2022-02-11 16:21:06 +0200

    v17, added chamfer distance, *1000 worst than without

--- models/vae.py
+++ models/vae.py
@@ -24,6 +24,38 @@ import cv2
 from model_utils import *
 from utils import *
 
+class ChamferLoss(nn.Module):
+
+    def __init__(self):
+        super(ChamferLoss, self).__init__()
+        self.use_cuda = torch.cuda.is_available()
+
+    def forward(self, preds, gts):
+        P = self.batch_pairwise_dist(gts, preds)
+        mins, _ = torch.min(P, 1)
+        loss_1 = torch.sum(mins)
+        mins, _ = torch.min(P, 2)
+        loss_2 = torch.sum(mins)
+        return loss_1 + loss_2
+
+    def batch_pairwise_dist(self, x, y):
+        bs, num_points_x, points_dim = x.size()
+        _, num_points_y, _ = y.size()
+        xx = torch.bmm(x, x.transpose(2, 1))
+        yy = torch.bmm(y, y.transpose(2, 1))
+        zz = torch.bmm(x, y.transpose(2, 1))
+        if self.use_cuda:
+            dtype = torch.cuda.LongTensor
+        else:
+            dtype = torch.LongTensor
+        diag_ind_x = torch.arange(0, num_points_x).type(dtype)
+        diag_ind_y = torch.arange(0, num_points_y).type(dtype)
+        rx = xx[:, diag_ind_x, diag_ind_x].unsqueeze(1).expand_as(
+            zz.transpose(2, 1))
+        ry = yy[:, diag_ind_y, diag_ind_y].unsqueeze(1).expand_as(zz)
+        P = rx.transpose(2, 1) + ry - 2 * zz
+        return P
+
 class VAE(pl.LightningModule):
     """ Class that implements a Variational Autoencoder """
 
@@ -515,14 +547,22 @@ class VAE(pl.LightningModule):
         regularizer that makes sure the distribution of the encoder and the decoder stay close.
         """
         # get the 2 losses
+        reconstruction_loss = ChamferLoss().cuda()
+        pcdgt = X.view(-1,3,128*128)
+        pcdgt = nn.functional.interpolate(pcdgt, size=(2048), mode='linear')
+        pcd_reco = X_hat.view(-1,3,128*128)
+        pcd_reco = nn.functional.interpolate(pcd_reco, size=(2048), mode='linear')
+        loss_cd = torch.sum(reconstruction_loss(pcdgt.permute(0, 2, 1) + 0.5,
+                                    pcd_reco.permute(0, 2, 1) + 0.5))
         data_fidelity_loss = VAE._data_fidelity_loss(X, X_hat)
         kl_divergence_loss = VAE._kl_divergence_loss(mean, std)
 
         # add them to compute the loss for each training example in the mini batch
-        loss = -data_fidelity_loss + kl_divergence_loss
+        loss = -data_fidelity_loss + kl_divergence_loss + loss_cd*1000
 
         # place them all inside a dictionary and return it
         losses = {"data_fidelity": torch.mean(data_fidelity_loss),
                   "kl-divergence": torch.mean(kl_divergence_loss),
+                  "CD": torch.mean(loss_cd),
                   "loss": torch.mean(loss)}
         return losses
